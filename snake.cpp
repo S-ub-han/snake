5a4d03caa5f8146bd07dd9822dad9365d9e9048c
Subhan Khan <136977903+S-ub-han@users.noreply.github.com>
2024-07-17 00:34:50 +0530

    Add files via upload

---
+++ snake.cpp
@@ -0,0 +1,180 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <unistd.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+
+using namespace std;
+
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+eDirection dir;
+
+void Setup() {
+    gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
+}
+
+void Draw() {
+    system("clear"); // Clear the console
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (j == 0)
+                cout << "#"; // Wall on the left
+            if (i == y && j == x)
+                cout << "O"; // Snake head
+            else if (i == fruitY && j == fruitX)
+                cout << "F"; // Fruit
+            else {
+                bool print = false;
+                for (int k = 0; k < nTail; k++) {
+                    if (tailX[k] == j && tailY[k] == i) {
+                        cout << "o"; // Snake body
+                        print = true;
+                    }
+                }
+                if (!print)
+                    cout << " ";
+            }
+
+            if (j == width - 1)
+                cout << "#"; // Wall on the right
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
+    cout << "Score: " << score << endl;
+}
+
+// Function to get a key press without waiting for Enter
+int _kbhit() {
+    static const int STDIN = 0;
+    static bool initialized = false;
+
+    if (!initialized) {
+        // Use termios to turn off line buffering
+        termios term;
+        tcgetattr(STDIN, &term);
+        term.c_lflag &= ~ICANON;
+        tcsetattr(STDIN, TCSANOW, &term);
+        setbuf(stdin, NULL);
+        initialized = true;
+    }
+
+    int bytesWaiting;
+    ioctl(STDIN, FIONREAD, &bytesWaiting);
+    return bytesWaiting;
+}
+
+int getch() {
+    int ch;
+    struct termios oldt, newt;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    return ch;
+}
+
+void Input() {
+    if (_kbhit()) {
+        switch (getch()) {
+            case 65: // Arrow up
+                dir = UP;
+                break;
+            case 66: // Arrow down
+                dir = DOWN;
+                break;
+            case 67: // Arrow right
+                dir = RIGHT;
+                break;
+            case 68: // Arrow left
+                dir = LEFT;
+                break;
+            case 'x':
+                gameOver = true;
+                break;
+        }
+    }
+}
+
+void Logic() {
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+    for (int i = 1; i < nTail; i++) {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+    switch (dir) {
+        case LEFT:
+            x--;
+            break;
+        case RIGHT:
+            x++;
+            break;
+        case UP:
+            y--;
+            break;
+        case DOWN:
+            y++;
+            break;
+        default:
+            break;
+    }
+
+    // If the snake runs into the walls
+    if (x >= width) x = 0; else if (x < 0) x = width - 1;
+    if (y >= height) y = 0; else if (y < 0) y = height - 1;
+
+    // If the snake runs into its own tail
+    for (int i = 0; i < nTail; i++)
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = true;
+
+    // If the snake eats the fruit
+    if (x == fruitX && y == fruitY) {
+        score += 10;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+        nTail++;
+    }
+}
+
+int main() {
+    srand(time(0));
+    Setup();
+    while (!gameOver) {
+        Draw();
+        Input();
+        Logic();
+        usleep(100000); // sleep for 100ms
+    }
+    return 0;
+}
